2a3bfaff5a3c4c08978037e69470e23c1b1832aa
Michael Dakin <4318690+michaeldakin@users.noreply.github.com>
2024-02-22 09:15:35 +1100

    feat(iteration): Added benchmark test

--- iteration/iterarion_test.go
+++ iteration/iterarion_test.go
@@ -9,10 +9,27 @@ func utilAssertCorrectValue(t *testing.T, expected, repeated string) {
 }
 
 func TestRepeat(t *testing.T) {
-	repeated := Repeat("a")
+	repeated := Repeat("a", 5)
 	expected := "aaaaa"
 
 	t.Run("expected 'a' chars repeated 5 times", func(t *testing.T) {
 		utilAssertCorrectValue(t, expected, repeated)
 	})
+}
+
+/*
+~/personal/go/go-with-tests/iteration ~/personal/go/go-with-tests
+goos: darwin
+goarch: arm64
+pkg: github.com/michaeldakin/go-with-tests/iteration
+BenchmarkTest-11        17173435                70.35 ns/op
+PASS
+ok      github.com/michaeldakin/go-with-tests/iteration 2.441s
+*/
+
+// go test -bench=.
+func BenchmarkTest(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a", 5)
+	}
 }
